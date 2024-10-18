d5738e98d67e53af8fe8f64646bd21daa91b2a4c
macbook <macbook@Ruslan-mac.local>
2024-10-18 20:52:38 +0400

    added new code

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿// #region homework
+    
+// Console.Write("Ededi daxil edin: ");
+// int n = int.Parse(Console.ReadLine());
+// int onlug = 0;
+// int teklik = n;
+//  do
+// {
+//      teklik /= 10;
+//      onlug++;
+// }
+// while (teklik != 0);
+
+// Console.WriteLine($"{n} Ededi {onlug} mertebelidir.");
+
+// #endregion 
+
+#region homework2
+
+Console.WriteLine("Ededi qeyd edin:");
+int eded = int.Parse(Console.ReadLine());
+if (eded <= 1)
+{
+    Console.WriteLine("Eded ne sade ne de murekkebdir!");
+}
+else if (eded <= 2)
+{
+    Console.WriteLine("Eded sade ededdir!");
+}
+else
+{
+    int regem = 2;
+    bool prime = true;
+
+    do 
+    {
+        if (eded % regem == 0)
+        {
+            prime = false;
+            break;
+        }
+        regem++;
+
+    } while (regem * regem <= eded);
+    if (prime)
+        Console.WriteLine($"{eded} Sade ededdir!");
+    else
+        Console.WriteLine($"{eded}  Murekkeb ededdir!");
+}
+    
+#endregion
